b1ef8b0ef9e9bf3284a92e7786383bc1dc130a8b
Mohammad-imran75 <imranbd7531@gmail.com>
2024-03-05 15:41:58 +0600

    added the show details button option

--- js/phone.js
+++ js/phone.js
@@ -22,7 +22,7 @@ const displayPhones = (phones,isShowAll) =>{
     
 
     phones.forEach(phone =>{
-        console.log(phone);
+        // console.log(phone);
          const PhoneCard = document.createElement('div');
          PhoneCard.classList = `card bg-gray-200 shadow-xl  border-2 border-gray-300 p-10 rounded-lg `
          PhoneCard.innerHTML = `
@@ -30,8 +30,8 @@ const displayPhones = (phones,isShowAll) =>{
         <div class="card-body ">
             <h2 class="card-title font-bold text-center">${phone.phone_name}</h2>
             <p class="text-center">There are many variations of passages of available, but the majority have suffered</p>
-            <div class="card-actions justify-end">
-            <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg text-white bg-[#0D6EFD]">Show Details</button>
+            <div class="card-actions justify-center">
+            <button onclick=handlerShowDetails('${phone.slug}') class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg text-white bg-[#0D6EFD] ">Show Details</button>
             </div>
         </div>
          `
@@ -55,6 +55,35 @@ const toggleLoadingSniper = (isLoading) =>{
     }else{
         loadingSniper.classList.add('hidden');
     }
+}
+
+const handlerShowDetails = async(id) =>{
+    console.log('click show detail',id);
+
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    const data = await res.json()
+    const phone = data.data;
+    showPhoneDetails(phone)
+    console.log(phone);
+}
+
+const showPhoneDetails = (phone) =>{
+
+
+    const showAllDetails = document.getElementById('phone-details-container');
+    showAllDetails.classList.add('space-y-4');
+    showAllDetails.innerHTML = `
+    <img class="text-center" src="${phone.image}" alt="" />
+    <h3 class="font-bold text-3xl">${phone.name}</h3>
+    <p class="font-semibold">Storage : ${phone.mainFeatures.storage
+    }</p>
+    <p class="font-semibold">Display size : ${phone?.display}</p>
+    <p class="font-semibold">Chipset : ${phone?.Chipset}</p>
+    <p class="font-semibold">
+    releaseDate : ${phone?.releaseDate}</p>
+    `
+
+    show_modal_details.showModal()
 }
 
 const showAllDevice = () =>{
